73b7156381e162bb520c2934b191f2ce80bfd69e
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-15 19:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ function getFormvalue() {
     //Write your code here
 	let firstName = document.querySelector('input[name="fname"]').value;
 	let lastName = document.querySelector('input[name="lname"]').value;
-		alert(firstName);
-		alert(lastName);
+		alert(firstName+" "+ lastName);
 }
